0190ea19631ad74456f9e4e97428173693e9fb89
pathtoresiliencebv <contact@pathtoresilience.nl>
2025-10-06 12:44:31 +0200

    Fix LoadError: Remove require_dependency for deleted authentication_helpers
    - Removed require_dependency 'spree/authentication_helpers' from initializer
    - This was trying to load the deleted lib/spree/authentication_helpers.rb
    - Caused: LoadError: cannot load such file -- spree/authentication_helpers
    - Spree's built-in authentication helpers work without custom override
    - Fixes build failure on Render

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -109,6 +109,4 @@ Spree.root_domain = ENV.fetch('SPREE_ROOT_DOMAIN', 'myaurelio.com')
 Spree.google_places_api_key = ENV['GOOGLE_PLACES_API_KEY'] if ENV['GOOGLE_PLACES_API_KEY'].present?
 Spree.screenshot_api_token = ENV['SCREENSHOT_API_TOKEN'] if ENV['SCREENSHOT_API_TOKEN'].present?
 
-Rails.application.config.to_prepare do
-  require_dependency 'spree/authentication_helpers'
-end
+# Authentication helpers removed - using Spree's built-in helpers
